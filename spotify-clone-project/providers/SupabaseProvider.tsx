f64d639be5e6a7469f04150b4e5f6cf5af3440e1
kei-1313 <candydog666@gmail.com>
2023-09-30 12:53:53 +0900

    データベースの型定義

--- spotify-clone-project/providers/SupabaseProvider.tsx
+++ spotify-clone-project/providers/SupabaseProvider.tsx
@@ -1,8 +1,10 @@
-"use client"
+"use client";
+import { useState } from "react";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 import { Database } from "@/types_db"
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-import { useState } from "react"
 
 interface SupabaseProviderProps {
   children: React.ReactNode
@@ -11,9 +13,15 @@ interface SupabaseProviderProps {
 const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
   children
 }) => {
-  cosnt[createClientComponentClient] = useState(() => {
-    createClientComponentClient<Database>
-  })
+  const [supabaseClient] = useState(() => 
+    createClientComponentClient<Database>()
+  )
 
-  
+  return(
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  )
 }
+
+export default SupabaseProvider
