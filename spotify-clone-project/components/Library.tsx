040ddbae5810f115b4640ad505194664d27c880c
kei-1313 <candydog666@gmail.com>
2023-10-01 16:59:37 +0900

    song upload機能実装

--- spotify-clone-project/components/Library.tsx
+++ spotify-clone-project/components/Library.tsx
@@ -6,6 +6,8 @@ import { useUser } from "@/hooks/useUser";
 import useAuthModal from "@/hooks/useAuthModal";
 import useUploadModal from "@/hooks/useUploadModal";
 
+
+
 const Library = () => {
   const { user } = useUser()
   const  authModal  = useAuthModal()
@@ -15,6 +17,8 @@ const Library = () => {
     if (!user) {
       return authModal.onOpen()
     }
+
+    return uploadModal.onOpen();
   }
 
   return (
@@ -26,7 +30,7 @@ const Library = () => {
             Your Library
           </p>
         </div>
-        <AiOutlinePlus onClick={onClick} size={20} className="text-neutral-400 cursor-pointer hover:text-white  transition" />
+        <AiOutlinePlus onClick={onClick} size={20} className="text-neutral-400 cursor-pointer hover:text-white  transition"/>
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-3">
         List of Songs
