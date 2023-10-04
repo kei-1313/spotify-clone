c227a764063a48a5a8656f0c7877c72a254785af
kei-1313 <candydog666@gmail.com>
2023-10-04 23:23:31 +0900

    いいねページでplayerを再生

--- spotify-clone-project/app/liked/components/LikedContent.tsx
+++ spotify-clone-project/app/liked/components/LikedContent.tsx
@@ -6,6 +6,7 @@ import MediaItem from '@/components/MediaItem'
 import { useUser } from '@/hooks/useUser'
 import { Song } from '@/types'
 import { useRouter } from 'next/navigation'
+import useOnPlay from '@/hooks/useOnPlay';
 
 interface LikedContentProps {
   songs: Song[]
@@ -16,7 +17,7 @@ const LikedContent: React.FC<LikedContentProps> = ({
 }) => {
   const router = useRouter()
   const { isLoading, user } = useUser();
-
+  const onPlay = useOnPlay(songs)
   useEffect(() => {
     if (!isLoading && !user) {
       router.replace('/');
@@ -47,7 +48,7 @@ const LikedContent: React.FC<LikedContentProps> = ({
           className="flex items-center gap-x-4 w-full"
         >
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={song} />
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
           <LikeButton songId={song.id} />
         </div>
